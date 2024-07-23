b09f412933095a1d83df9b4c72d7f7cfe156bc16
DIGIART <divyshah1907@gmail.com>
2024-07-23 17:00:51 +0530

    done

--- db/config.js
+++ db/config.js
@@ -1,3 +1,18 @@
+// const mongoose = require('mongoose');
+// const dotenv = require('dotenv');
+
+// dotenv.config();  // Load environment variables from .env file
+
+// const dbHOST = process.env.DBHOST;
+
+// mongoose.connect(dbHOST, { useNewUrlParser: true, useUnifiedTopology: true })
+//     .then(() => {
+//         console.log('MongoDB Connected...');
+//     })
+//     .catch((err) => {
+//         console.error('Error while connecting to MongoDB:', err);
+//     });
+
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
@@ -5,7 +20,12 @@ dotenv.config();  // Load environment variables from .env file
 
 const dbHOST = process.env.DBHOST;
 
-mongoose.connect(dbHOST, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(dbHOST, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+})
     .then(() => {
         console.log('MongoDB Connected...');
     })
--- index.js
+++ index.js
@@ -1,3 +1,53 @@
+// const express = require('express');
+// const cors = require("cors");
+// const dotenv = require('dotenv');
+// const mongoose = require('mongoose');
+// const multer = require('multer');
+// require('./db/config');
+// const User = require("./db/User");
+// const { Await } = require('react-router-dom');
+
+
+// dotenv.config();
+// const app = express();
+
+// app.use(express.json());
+// app.use(cors());
+
+// app.get('/', (req, res) => {
+//     res.send('Products API running');
+// });
+
+
+// app.post("/register",async (req,resp)=>{
+//      let user= new User(req.body);
+//      let result =await user.save();
+//      result = result.toObject();
+//      delete result.password;
+//      resp.send(result);
+// });
+
+// app.post("/login", async (req,resp)=>{
+//     console.log(req.body)
+//     if(req.body.password && req.body.name) {
+//     let user = await User.findOne(req.body).select("-password");
+//     if(user)
+//      {
+//         resp.send(user)
+//      }else{
+//         resp.send({result : 'No User Found'})
+//      }
+//     } else {
+//         resp.send({result : 'No User Found'})
+//     }
+
+// })
+
+// const PORT = process.env.PORT || 5001;
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
+
 const express = require('express');
 const cors = require("cors");
 const dotenv = require('dotenv');
@@ -5,8 +55,8 @@ const mongoose = require('mongoose');
 const multer = require('multer');
 require('./db/config');
 const User = require("./db/User");
+console.log("User model loaded successfully");
 const { Await } = require('react-router-dom');
-
 
 dotenv.config();
 const app = express();
@@ -18,30 +68,27 @@ app.get('/', (req, res) => {
     res.send('Products API running');
 });
 
-
-app.post("/register",async (req,resp)=>{
-     let user= new User(req.body);
-     let result =await user.save();
-     result = result.toObject();
-     delete result.password;
-     resp.send(result);
+app.post("/register", async (req, resp) => {
+    let user = new User(req.body);
+    let result = await user.save();
+    result = result.toObject();
+    delete result.password;
+    resp.send(result);
 });
 
-app.post("/login", async (req,resp)=>{
-    console.log(req.body)
-    if(req.body.password && req.body.name) {
-    let user = await User.findOne(req.body).select("-password");
-    if(user)
-     {
-        resp.send(user)
-     }else{
-        resp.send({result : 'No User Found'})
-     }
+app.post("/login", async (req, resp) => {
+    console.log(req.body);
+    if (req.body.password && req.body.name) {
+        let user = await User.findOne(req.body).select("-password");
+        if (user) {
+            resp.send(user);
+        } else {
+            resp.send({ result: 'No User Found' });
+        }
     } else {
-        resp.send({result : 'No User Found'})
+        resp.send({ result: 'No User Found' });
     }
-
-})
+});
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
